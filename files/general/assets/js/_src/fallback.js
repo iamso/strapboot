5790a62a7cec01aa0e3464796e164cd86640a68c
iamso <so@dev.so>
2016-09-25 17:08:14 +0200

    check for promise support

--- files/general/assets/js/_src/fallback.js
+++ files/general/assets/js/_src/fallback.js
@@ -23,4 +23,7 @@
   // check for jQuery presence
   !!window.jQuery || document.write('<script src="' + (base || '') + '/assets/js/vendor/jquery.min.js"><\/script>');
 
+  // check for Promise support
+  !!window.Promise || document.write('<script src="' + (base || '') + '/assets/js/vendor/promise.min.js"><\/script>');
+
 });
