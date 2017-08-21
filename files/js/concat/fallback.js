974dee96ee6350ac26c05128a1929c04e0cf0534
iamso <so@dev.so>
2017-08-21 11:14:49 +0200

    add fallback for requestIdleCallback

--- files/js/concat/fallback.js
+++ files/js/concat/fallback.js
@@ -18,4 +18,24 @@
     window.location.origin = window.location.protocol + "//" + window.location.hostname + (window.location.port ? ':' + window.location.port: '');
   }
 
+  window.requestIdleCallback =
+    window.requestIdleCallback ||
+    function(cb) {
+      var start = Date.now();
+      return setTimeout(function() {
+        cb({
+          didTimeout: false,
+          timeRemaining: function() {
+            return Math.max(0, 50 - (Date.now() - start));
+          }
+        });
+      }, 1);
+    }
+
+  window.cancelIdleCallback =
+    window.cancelIdleCallback ||
+    function(id) {
+      clearTimeout(id);
+    }
+
 })(window, document);
--- files/js/webpack/fallback.js
+++ files/js/webpack/fallback.js
@@ -9,3 +9,23 @@ const base = document.querySelector('#fallback-js').dataset.base;
 if (!window.location.origin) {
   window.location.origin = window.location.protocol + "//" + window.location.hostname + (window.location.port ? ':' + window.location.port: '');
 }
+
+window.requestIdleCallback =
+  window.requestIdleCallback ||
+  function(cb) {
+    const start = Date.now();
+    return setTimeout(() => {
+      cb({
+        didTimeout: false,
+        timeRemaining: () => {
+          return Math.max(0, 50 - (Date.now() - start));
+        }
+      });
+    }, 1);
+  }
+
+window.cancelIdleCallback =
+  window.cancelIdleCallback ||
+  function(id) {
+    clearTimeout(id);
+  }
