fb0bf0784281faca80edbec93b507fe6f0b8118f
iamso <so@dev.so>
2017-08-21 11:15:07 +0200

    add script loading util

--- files/js/webpack/modules/utils.js
+++ files/js/webpack/modules/utils.js
@@ -78,3 +78,25 @@ export function sleep(time = 1000) {
     setTimeout(resolve, time);
   });
 }
+
+// load script dynamically
+export function loadScript(src) {
+  return new Promise((resolve, reject) => {
+    if (document.querySelector(`script.loaded[src="${src}"]:not(.error)`)) {
+      resolve();
+      return;
+    }
+    const script = document.createElement('script');
+    script.async = true;
+    script.onload = (e) => {
+      script.classList.add('loaded');
+      resolve();
+    };
+    script.onerror = (e) => {
+      script.classList.add('error');
+      reject();
+    };
+    script.src = src;
+    document.body.appendChild(script);
+  });
+}
