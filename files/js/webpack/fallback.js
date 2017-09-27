c551f9f0548f3c9f26622ce310e8e835a17a74cb
iamso <so@dev.so>
2017-09-27 21:16:08 +0200

    update fallback.js

--- files/js/webpack/fallback.js
+++ files/js/webpack/fallback.js
@@ -1,15 +1,26 @@
-const base = document.querySelector('#fallback-js').dataset.base;
+// current script src dir
+const src = [].slice.call(document.querySelectorAll('script'), 0).pop().src.replace(/\/[^\/]+$/, '');
+
+// demonstrate javascript support
+const html = document.documentElement;
+html.classList.remove('no-js');
+html.classList.add('js');
 
 // check for jQuery presence
-!!window.jQuery || document.write('<script src="' + (base || '') + '/assets/js/vendor/jquery.min.js"><\/script>');
+!!window.jQuery || document.write(`<script src="${src}/vendor/jquery.min.js"><\/script>`);
 
 // check for Promise support
-!!window.Promise || document.write('<script src="' + (base || '') + '/assets/js/vendor/promise.min.js"><\/script>');
+!!window.Promise || document.write(`<script src="${src}/vendor/promise.min.js"><\/script>`);
 
+// check for object-fit support
+!!('objectFit' in html.style) || (html.classList.add('no-objectfit'), document.write(`<script src="${src}/vendor/objectFitPolyfill.basic.min.js"><\/script>`));
+
+// check for location origin support (mostly IE)
 if (!window.location.origin) {
   window.location.origin = window.location.protocol + "//" + window.location.hostname + (window.location.port ? ':' + window.location.port: '');
 }
 
+// fallback for requestIdleCallback
 window.requestIdleCallback =
   window.requestIdleCallback ||
   function(cb) {
@@ -24,6 +35,7 @@ window.requestIdleCallback =
     }, 1);
   }
 
+// fallback for cancelIdleCallback
 window.cancelIdleCallback =
   window.cancelIdleCallback ||
   function(id) {
