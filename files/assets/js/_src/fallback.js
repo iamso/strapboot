84344fb208afbf3556618aeb98f4436a70352d18
iamso <so@dev.so>
2018-12-21 15:58:05 +0100

    wrap fallback in iife

--- files/assets/js/_src/fallback.js
+++ files/assets/js/_src/fallback.js
@@ -1,47 +1,51 @@
-// current script src dir
-const src = [].slice.call(document.querySelectorAll('script'), 0).pop().src.replace(/\/[^\/]+$/, '');
+(function(window, document){
 
-// demonstrate javascript support
-const html = document.documentElement;
-html.classList.remove('no-js');
-html.classList.add('js');
+  // current script src dir
+  const src = [].slice.call(document.querySelectorAll('script'), 0).pop().src.replace(/\/[^\/]+$/, '');
 
-// check for jQuery presence
-// !!window.jQuery || document.write(`<script src="${src}/vendor/jquery.min.js"><\/script>`);
+  // demonstrate javascript support
+  const html = document.documentElement;
+  html.classList.remove('no-js');
+  html.classList.add('js');
 
-// check for Promise support
-!!window.Promise || document.write(`<script src="${src}/vendor/promise.min.js"><\/script>`);
+  // check for jQuery presence
+  // !!window.jQuery || document.write(`<script src="${src}/vendor/jquery.min.js"><\/script>`);
 
-// check for object-fit support
-// !!('objectFit' in html.style) || (html.classList.add('no-objectfit'), document.write(`<script src="${src}/vendor/objectFitPolyfill.basic.min.js"><\/script>`));
-!!('objectFit' in html.style) || html.classList.add('no-objectfit');
+  // check for Promise support
+  !!window.Promise || document.write(`<script src="${src}/vendor/promise.min.js"><\/script>`);
 
-// check for IntersectionObserver support
-!!window.IntersectionObserver || document.write(`<script src="${src}/vendor/intersection-observer.js"><\/script>`);
+  // check for object-fit support
+  // !!('objectFit' in html.style) || (html.classList.add('no-objectfit'), document.write(`<script src="${src}/vendor/objectFitPolyfill.basic.min.js"><\/script>`));
+  !!('objectFit' in html.style) || html.classList.add('no-objectfit');
 
-// check for location origin support (mostly IE)
-if (!window.location.origin) {
-  window.location.origin = `${window.location.protocol}//${window.location.hostname}${window.location.port ? `:${window.location.port}` : ''}`;
-}
+  // check for IntersectionObserver support
+  !!window.IntersectionObserver || document.write(`<script src="${src}/vendor/intersection-observer.js"><\/script>`);
 
-// fallback for requestIdleCallback
-window.requestIdleCallback =
-  window.requestIdleCallback ||
-  function(cb) {
-    const start = Date.now();
-    return setTimeout(() => {
-      cb({
-        didTimeout: false,
-        timeRemaining: () => {
-          return Math.max(0, 50 - (Date.now() - start));
-        }
-      });
-    }, 1);
-  };
+  // check for location origin support (mostly IE)
+  if (!window.location.origin) {
+    window.location.origin = `${window.location.protocol}//${window.location.hostname}${window.location.port ? `:${window.location.port}` : ''}`;
+  }
 
-// fallback for cancelIdleCallback
-window.cancelIdleCallback =
-  window.cancelIdleCallback ||
-  function(id) {
-    clearTimeout(id);
-  };
+  // fallback for requestIdleCallback
+  window.requestIdleCallback =
+    window.requestIdleCallback ||
+    function(cb) {
+      const start = Date.now();
+      return setTimeout(() => {
+        cb({
+          didTimeout: false,
+          timeRemaining: () => {
+            return Math.max(0, 50 - (Date.now() - start));
+          }
+        });
+      }, 1);
+    };
+
+  // fallback for cancelIdleCallback
+  window.cancelIdleCallback =
+    window.cancelIdleCallback ||
+    function(id) {
+      clearTimeout(id);
+    };
+
+})(window, document);
